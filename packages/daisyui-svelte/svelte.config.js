952adcdfd84aafc4d367485f16c2ca1508936763
Nikolas Silva <nikolas.rsilva@gmail.com>
2023-06-07 05:31:53 -0300

    Initial commit

---
+++ packages/daisyui-svelte/svelte.config.js
@@ -0,0 +1,16 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		files: {
+			lib: 'src'
+		}
+	}
+};
+
+export default config;
